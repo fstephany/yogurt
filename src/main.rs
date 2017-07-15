fcdca2606eb32312e5f8f0f0095d3940fbfed6d4
Francois Stephany <francois@tamere.eu>
2017-07-15 10:41:08 +0200

    Create workspace directories

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 use std::path::*;
 
 
@@ -12,7 +13,7 @@ struct Vm {
     bitness: Bitness,
 }
 
-fn get_yogurt_workspace_path() ->  Option<PathBuf> {
+fn yogurt_workspace_path() ->  Option<PathBuf> {
     match env::home_dir() {
         Some(mut home_dir) => {
             home_dir.push(".yogurt");
@@ -22,21 +23,54 @@ fn get_yogurt_workspace_path() ->  Option<PathBuf> {
     }
 }
 
-fn get_available_vms() -> Vec<Vm> {
+fn vms_dir() -> PathBuf {
+    yogurt_workspace_path()
+        .expect("Could not find workspace directory")
+        .join("vms")
+}
+
+fn images_dir() -> PathBuf {
+    yogurt_workspace_path()
+        .expect("Could not find workspace directory")
+        .join("images")
+}
+
+fn sources_dir() -> PathBuf {
+    yogurt_workspace_path()
+        .expect("Could not find workspace directory")
+        .join("sources")
+}
+
+fn create_dirs_if_necessary() {
+    fs::create_dir_all(vms_dir()).expect("Could not create VMs directory. Abort");
+    fs::create_dir_all(images_dir()).expect("Could not create images directory. Abort");
+    fs::create_dir_all(sources_dir()).expect("Could not create sources directory. Abort");
+}
+
+fn available_vms() -> Vec<Vm> {
     let mut vms = Vec::new();
-    vms.push(Vm{ name: String::from("20170708"), bitness: Bitness::B64 });
+    let dirs_to_scan = fs::read_dir(vms_dir()).unwrap();
+
+    for entry in dirs_to_scan {
+        let path = entry.unwrap().path();
+        let dir_name = path.file_name().unwrap().to_str().unwrap();
+
+        if path.is_dir() {
+            vms.push(Vm{ name: String::from(dir_name), bitness: Bitness::B64});
+        }
+    }
 
     return vms
 }
 
-fn main() {
-    match get_yogurt_workspace_path() {
-        Some(path) => println!("Yogurt storage: {:?}", path),
-        None => panic!("Could not find Yogurt working directory. Abort    ")
-    }
-
+fn print_available_vms() {
     println!("Available VMs:");
-    for vm in get_available_vms() {
+    for vm in available_vms() {
         println!("- {}", vm.name);
     }
+}
+
+fn main() {
+    create_dirs_if_necessary();
+    print_available_vms();
 }
